6caa403b8ae91958239fd841e421741c3a8b2bc4
headebeast <headesabah@gmail.com>
2018-09-12 17:07:16 +0300

    done

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SideNavBar from '../components/SideNavBar'
+
+const IndexPage = () => (
+  <div>
+    <SideNavBar></SideNavBar>
+  </div>
+)
+
+export default IndexPage;
